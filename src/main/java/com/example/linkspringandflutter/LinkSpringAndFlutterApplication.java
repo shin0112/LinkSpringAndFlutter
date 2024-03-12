476682673e9f1a63c93145ba7f7c482a074a9473
shin0112 <wnslcosltimo12@gmail.com>
2024-03-13 01:28:11 +0900

    [chore] spring boot initial settings

---
+++ src/main/java/com/example/linkspringandflutter/LinkSpringAndFlutterApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkspringandflutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSpringAndFlutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkSpringAndFlutterApplication.class, args);
+    }
+
+}
